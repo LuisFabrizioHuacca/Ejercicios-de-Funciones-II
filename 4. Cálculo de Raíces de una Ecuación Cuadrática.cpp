94ad4383837a166888f1efccef722ba960e78d9e
LuisHuacca <fhuacca@gmail.com>
2024-06-26 01:11:37 -0500

    Se ha añadido el ejercicio 4. Cálculo de Raíces de una Ecuación Cuadrática

---
+++ 4. Cálculo de Raíces de una Ecuación Cuadrática.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+#include <complex>
+
+using namespace std;
+
+int main() {
+    double a, b, c;
+
+	cout<<"El programa se ha iniciado correctamente"<<endl;
+	
+    cout<<"Introduce el coeficiente a: ";
+    cin>>a;
+
+    cout<< "Introduce el coeficiente b: ";
+    cin>>b;
+
+    cout<<"Introduce el coeficiente c: ";
+    cin>>c;
+
+    double discriminante=b*b-4*a*c;
+
+    if (discriminante>0) {
+        double raiz1=(-b + sqrt(discriminante)) / (2 * a);
+        double raiz2=(-b - sqrt(discriminante)) / (2 * a);
+        cout<<"Las raíces son reales y distintas." << endl;
+        cout<<"Raíz 1: "<<raiz1<<endl;
+        cout<<"Raíz 2: "<<raiz2<<endl;
+    } else if (discriminante == 0) {
+        double raiz = -b / (2 * a);
+        cout<<"Las raíces son reales y iguales."<<endl;
+        cout<<"Raíz: "<<raiz<<endl;
+    } else {
+        complex<double> raiz1 = complex<double>(-b, sqrt(-discriminante)) / (2.0 * a);
+        complex<double> raiz2 = complex<double>(-b, -sqrt(-discriminante)) / (2.0 * a);
+        cout<<"Las raíces son complejas y distintas."<<endl;
+        cout<<"Raíz 1: "<<raiz1<<endl;
+        cout<<"Raíz 2: "<<raiz2<<endl;
+    }
+
+    return 0;
+}
+
