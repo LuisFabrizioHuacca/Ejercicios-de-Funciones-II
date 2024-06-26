c8ce2b499f2ab82f9cab08f37064baae46825562
LuisHuacca <fhuacca@gmail.com>
2024-06-25 23:46:48 -0500

    Se ha cambiado una letra con tilde, acortado textos e implementado un mensaje de inicio

--- 3. Cálculo de la Hipotenusa.cpp
+++ 3. Cálculo de la Hipotenusa.cpp
@@ -5,16 +5,18 @@ using namespace std;
 
 int main() {
     double cateto1, cateto2, hipotenusa;
+    
+    cout<<"El programa se ha iniciado correctamente"<<endl;
 
-    cout<<"Introduce el valor del primer cateto: ";
+    cout<<"Introduce el primer cateto: ";
     cin>>cateto1;
 
-    cout<<"Introduce el valor del segundo cateto: ";
+    cout<<"Introduce el segundo cateto: ";
     cin>>cateto2;
 
     hipotenusa=sqrt((cateto1 * cateto1) + (cateto2 * cateto2));
 
-    cout<<"La hipotenusa del triángulo es: "<<hipotenusa<<endl;
+    cout<<"La hipotenusa del triangulo es: "<<hipotenusa<<endl;
 
     return 0;
 }
